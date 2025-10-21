a7f01fcc16b24a55b0256929faa0f734dc18cc82
kaylasalsabillah78-ctrl <kaylasalsabillah78@gmail.com>
2025-10-21 13:20:58 +0700

    Add click event listener to greeting button

---
+++ script.js
@@ -0,0 +1,7 @@
+/1.pilih tombol berdasarkan ID nya
+const tombol Sapa = document.getElementByld('sapaButton'):
+/2.Tambahkan'event listener'untuk aksi 'click'
+tombolsapa.addEventListenner('click',function() (
+  //3.Jalankan kode ini ketika tombol di klik
+ alert ('Halo Terima kasih sudah berkunjung!')
+  );
